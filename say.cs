c77a1deafe01ee99fda3f3bb3eea4440a8839b8c
Jaehoon You <teslamint@gmail.com>
2019-06-21 17:45:56 +0900

    Create say.cs

---
+++ say.cs
@@ -0,0 +1,10 @@
+// how to compile and run(use mono): mcs say.cs; mono say.exe
+using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("일하기 싫다");
+    }
+}
